6b73c1c2807192cb01aa19ed67be699c7b75901c
Harminder Singh <harminder.6337@gmail.com>
2025-11-22 20:02:54 +0530

    doing proper alignment of code.

--- src/streamsApi/FirstNonRepeatingCharacterFromGivenString.java
+++ src/streamsApi/FirstNonRepeatingCharacterFromGivenString.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-/**  Find the First Non Repeating Character in Given String ,
+/**  Find the First Non-Repeating Character in Given String ,
     and Logic should be Dynamic not according to static string input */
 
 public class FirstNonRepeatingCharacterFromGivenString {
